30cf58ace5220184a16bcf41bedf60a95d6de998
CWDE615 <cwellis001@gmail.com>
2021-10-17 15:44:34 -0400

    implemented HypotheticalBuyableFuelActivity

--- app/src/main/java/com/example/a45mph/HypotheticalBuyableFuelActivity.java
+++ app/src/main/java/com/example/a45mph/HypotheticalBuyableFuelActivity.java
@@ -3,6 +3,7 @@ package com.example.a45mph;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HypotheticalBuyableFuelActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class HypotheticalBuyableFuelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hypothetical_buyable_fuel);
+    }
+
+    public double calculateHypotheticalBuyableFuel(View V)
+    {
+        double result = -1;
+
+        try {
+            result = FuelCalculators.hypotheticalBuyableFuel(0.0,0.0);
+        } catch (ArithmeticException e) {
+            // negative inputs not allowed here
+        } catch (Exception f) {
+            // something else wet wrong
+        }
+
+        return result;
     }
 }
