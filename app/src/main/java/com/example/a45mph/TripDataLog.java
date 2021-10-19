dafcdfdbe409b3fe5e5993e95fb45bbd5e812b4d
kieranford4805 <kieran.ford@ufl.edu>
2021-10-19 10:28:02 -0400

    Transfer 1st attempts

--- app/src/main/java/com/example/a45mph/TripDataLog.java
+++ app/src/main/java/com/example/a45mph/TripDataLog.java
@@ -1,5 +1,9 @@
 package com.example.a45mph;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class TripDataLog extends DataLog {
@@ -33,4 +37,18 @@ public class TripDataLog extends DataLog {
     public double getConsumption() { return consumption; }
     public double getOdometer() { return odometer; }
     public double getMileage() { return mileage; }
+
+    public void transfer() throws IOException {
+        File tempFile = new File("tripLog.csv");
+        if(tempFile.exists() == false){
+            BufferedWriter bf = new BufferedWriter((new FileWriter("tripLog.csv")));
+            bf.write(time + "," + consumption + "," + odometer + "," + mileage);
+            bf.close();
+        }
+        else {
+            FileWriter fw = new FileWriter("tripLog.csv", true);
+            fw.write("," + time + "," + consumption + "," + odometer + "," + mileage);
+            fw.close();
+        }
+    }
 }
