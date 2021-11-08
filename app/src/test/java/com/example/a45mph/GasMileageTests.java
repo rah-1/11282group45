f467b723f48bc309fa38ed9652c88b172571a12f
kieranford4805 <kieran.ford@ufl.edu>
2021-11-08 11:18:28 -0500

    Created Gas Mileage Calculator

--- app/src/test/java/com/example/a45mph/GasMileageTests.java
+++ app/src/test/java/com/example/a45mph/GasMileageTests.java
@@ -5,28 +5,28 @@ import org.junit.Test;
 public class GasMileageTests {
     @Test
     public void executeGasMileage(){
-        assert(30 / 2 == FuelCalculators.gasMileage(2, 30));
-        assert(240 / 40 == FuelCalculators.gasMileage(40, 220));
-        assert(5 / 25 == FuelCalculators.gasMileage(25, 5));
+        assert(30 / 2 == FuelCalculators.gasMileage( 30,2));
+        assert(240 / 40 == FuelCalculators.gasMileage(220,40));
+        assert(25 / 5 == FuelCalculators.gasMileage(25, 5));
     }
 
     @Test
     public void testGasMileageRounding(){
-        assert(0.51 == FuelCalculators.gasMileage(1, 0.506));
-        assert(4.77 == FuelCalculators.gasMileage(1, 4.772));
+        assert(0.51 == FuelCalculators.gasMileage(0.506, 1));
+        assert(4.77 == FuelCalculators.gasMileage(4.772, 1));
     }
 
     @Test
     public void testGasMileageZero(){
         try{
-            double badInput = FuelCalculators.gasMileage(0,5);
+            double badInput = FuelCalculators.gasMileage(5,0);
             assert false;
         }
         catch (ArithmeticException e){
             assert true;
         }
         try{
-            double badInput = FuelCalculators.gasMileage(5,0);
+            double badInput = FuelCalculators.gasMileage(0,5);
             assert false;
         }
         catch (ArithmeticException e){
@@ -44,21 +44,21 @@ public class GasMileageTests {
     @Test
     public void testGasMileageNegative(){
         try{
-            double badInput = FuelCalculators.gasMileage(7,-9);
+            double badInput = FuelCalculators.gasMileage(-9, 7);
             assert false;
         }
         catch (ArithmeticException e){
             assert true;
         }
         try{
-            double badInput = FuelCalculators.gasMileage(-7,9);
+            double badInput = FuelCalculators.gasMileage(9, -7);
             assert false;
         }
         catch (ArithmeticException e){
             assert true;
         }
         try{
-            double badInput = FuelCalculators.gasMileage(-7,-9);
+            double badInput = FuelCalculators.gasMileage(-9, -7);
             assert false;
         }
         catch (ArithmeticException e){
