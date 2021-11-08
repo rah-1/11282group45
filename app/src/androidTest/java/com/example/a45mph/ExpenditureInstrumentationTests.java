583c5a856e115510c23f3381ce14a464c67b5c3e
CWDE615 <cwellis001@gmail.com>
2021-11-07 20:56:20 -0500

    Forgot to add the test file from 2 commits ago

---
+++ app/src/androidTest/java/com/example/a45mph/ExpenditureInstrumentationTests.java
@@ -0,0 +1,87 @@
+package com.example.a45mph;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class ExpenditureInstrumentationTests {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.a45mph", appContext.getPackageName());
+    }
+
+    @Test
+    public void testExpenditureRecording1() {
+
+        try {
+            InstrumentationTestHelper.setUpFile(ExpenditureDataLog.FILEPATH);
+            LocalDateTime thisInstant = LocalDateTime.now();
+            VehicleProfile testCar = new VehicleProfile();
+            ExpenditureDataLog log = new ExpenditureDataLog(10.00,22,thisInstant,testCar);
+            log.transfer();
+
+            Scanner s = new Scanner(new File(ExpenditureDataLog.FILEPATH));
+            InstrumentationTestHelper.testTransfer(log,s);
+            assert !s.hasNextLine();
+
+        } catch (Exception e) {
+            InstrumentationTestHelper.exceptionHandler(e);
+        }
+
+    }
+
+    @Test
+    public void testExpenditureRecording2() {
+
+        try {
+            InstrumentationTestHelper.setUpFile(ExpenditureDataLog.FILEPATH);
+            LocalDateTime thisInstant = LocalDateTime.now();
+            VehicleProfile testCar = new VehicleProfile();
+            ExpenditureDataLog log = new ExpenditureDataLog(10.00,22,thisInstant,testCar);
+            ExpenditureDataLog log1 = new ExpenditureDataLog(23.94,12,thisInstant,testCar);
+            log.transfer();
+            log1.transfer();
+
+            Scanner s = new Scanner(new File(ExpenditureDataLog.FILEPATH));
+            InstrumentationTestHelper.testTransfer(log,s);
+            InstrumentationTestHelper.testTransfer(log1,s);
+            assert !s.hasNextLine();
+
+        } catch (Exception e) {
+            InstrumentationTestHelper.exceptionHandler(e);
+        }
+    }
+
+    @Test
+    public void testExpenditureRecording3() {
+
+        try {
+            InstrumentationTestHelper.setUpFile(ExpenditureDataLog.FILEPATH);
+            LocalDateTime thisInstant = LocalDateTime.now();
+            VehicleProfile testCar = new VehicleProfile();
+            ExpenditureDataLog log = new ExpenditureDataLog(10.00,22,thisInstant,testCar);
+            ExpenditureDataLog log1 = new ExpenditureDataLog(23.94,12,thisInstant,testCar);
+
+            assertEquals(log.getExpenditure(),10.00,0);
+            assertEquals(log.getAmountBought(), 22.00, 0);
+            assertEquals(log.getTime(), thisInstant);
+            assertEquals(log.getVehicle().getName(), testCar.getName());
+
+        } catch (Exception e) {
+            InstrumentationTestHelper.exceptionHandler(e);
+        }
+    }
+
+
+}
