9329418a3f55f4ea13dfb331b0a7af8bc32cd74d
rah-1 <65043687+rah-1@users.noreply.github.com>
2021-11-08 19:24:33 -0500

    early stages of vehicle profiles

--- app/src/main/java/com/example/a45mph/EnvironmentalTrackingActivity.java
+++ app/src/main/java/com/example/a45mph/EnvironmentalTrackingActivity.java
@@ -2,15 +2,32 @@ package com.example.a45mph;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.widget.Button;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class EnvironmentalTrackingActivity extends AppCompatActivity {
+    private Button personalizedReportsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_environmental_tracking);
+
+        personalizedReportsButton = (Button) findViewById(R.id.personalizedreportsbutton);
+        personalizedReportsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                personalizedReportsActivity();
+            }
+        });
     }
 
+    public void personalizedReportsActivity() {
+        Intent intent = new Intent(this, PersonalizedReportsActivity.class);
+        startActivity(intent);
+    }
     // devise methods for viewing environmental impact
 }
