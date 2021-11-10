646b703f6b566cec0d0c4379944a26f01107c600
CWDE615 <cwellis001@gmail.com>
2021-11-09 23:27:43 -0500

    Implement Tests for adding Expenditures and Trips to the Adapter

--- app/src/androidTest/java/com/example/a45mph/RecordPresentationInstrumentationTests.java
+++ app/src/androidTest/java/com/example/a45mph/RecordPresentationInstrumentationTests.java
@@ -95,13 +95,78 @@ public class RecordPresentationInstrumentationTests {
     }
 
     @Test
-    public void testAddTrip() {
+    public void testAddingTrip() {
+        try {
+            assert InstrumentationTestHelper.setUpFile(TripDataLog.FILEPATH);
+            LocalDateTime thisInstant = LocalDateTime.now();
+            VehicleProfile testCar = new VehicleProfile();
 
+            TripDataLog trip1 = new TripDataLog(10,42, thisInstant, testCar);
+            TripDataLog trip2 = new TripDataLog(10, 23, thisInstant, testCar);
+            TripDataLog trip3 = new TripDataLog(49, 334, thisInstant,testCar);
+            TripDataLog trip4 = new TripDataLog(38.44,134.5, thisInstant, testCar);
+            TripDataLog trip5 = new TripDataLog(902, 33, thisInstant, testCar);
+            TripDataLog list[] = {trip1,trip2,trip3,trip4,trip5};
+
+            TripDataLogAdapter tripAdapter = new TripDataLogAdapter();
+
+            for (TripDataLog t : list)
+            {
+                RecordTripActivity.recordTrip(t.getOdometer(),t.getConsumption(),t.getTime(),t.getVehicle());
+            }
+
+            ArrayList<TripDataLog> list2 = tripAdapter.getTrips();
+
+            int i = 0;
+            for (TripDataLog t : list2)
+            {
+                t.setEntry();
+                assertEquals(t.entry,list[i++].entry);
+            }
+
+        } catch (ArithmeticException e) {
+            assert InstrumentationTestHelper.exceptionHandler(e);
+        } catch (IOException e) {
+            assert InstrumentationTestHelper.exceptionHandler(e);
+        }
     }
 
     @Test
-    public void testAddExpend() {
+    public void testAddingExpenditure() {
+        try {
+            assert InstrumentationTestHelper.setUpFile(ExpenditureDataLog.FILEPATH);
+            LocalDateTime thisInstant = LocalDateTime.now();
+            VehicleProfile testCar = new VehicleProfile();
 
+            ExpenditureDataLog exp1 = new ExpenditureDataLog(10,42, thisInstant, testCar);
+            ExpenditureDataLog exp2 = new ExpenditureDataLog(10, 23, thisInstant, testCar);
+            ExpenditureDataLog exp3 = new ExpenditureDataLog(49, 334, thisInstant,testCar);
+            ExpenditureDataLog exp4 = new ExpenditureDataLog(38.44,134.5, thisInstant, testCar);
+            ExpenditureDataLog exp5 = new ExpenditureDataLog(902, 33, thisInstant, testCar);
+            ExpenditureDataLog list[] = {exp1,exp2,exp3,exp4,exp5};
+
+            ExpenditureDataLogAdapter expendAdapter = new ExpenditureDataLogAdapter();
+
+            for (ExpenditureDataLog e : list)
+            {
+                FuelCalculators.fuelCost(e.getExpenditure(),e.getAmountBought(),e.getTime(),e.getVehicle());
+                e.transfer();
+            }
+
+            ArrayList<ExpenditureDataLog> list2 = expendAdapter.getExpenditures();
+
+            int i = 0;
+            for (ExpenditureDataLog e : list2)
+            {
+                e.setEntry();
+                assertEquals(e.entry,list[i++].entry);
+            }
+
+        } catch (ArithmeticException e) {
+            assert InstrumentationTestHelper.exceptionHandler(e);
+        } catch (IOException e) {
+            assert InstrumentationTestHelper.exceptionHandler(e);
+        }
     }
 
     @Test
