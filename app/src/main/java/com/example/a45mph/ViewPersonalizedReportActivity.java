db4b80d6feab45f18a867819fd093e824b2c892e
rah-1 <65043687+rah-1@users.noreply.github.com>
2021-11-13 01:47:22 -0500

    CEN-14: Create Some Functionality for Viewing Personalized Reports

--- app/src/main/java/com/example/a45mph/ViewPersonalizedReportActivity.java
+++ app/src/main/java/com/example/a45mph/ViewPersonalizedReportActivity.java
@@ -3,12 +3,28 @@ package com.example.a45mph;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class ViewPersonalizedReportActivity extends AppCompatActivity {
+    private TextView startDate, endDate, perfRating, TFConsumption, TFCost, avgMileage, enviroImpact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_personalized_report);
+
+        startDate = (TextView) findViewById(R.id.dateone);
+        endDate = (TextView) findViewById(R.id.datetwo);
+        perfRating = (TextView) findViewById(R.id.starstextview);
+        TFConsumption = (TextView) findViewById(R.id.totalfuelcosttextview);
+        TFCost = (TextView) findViewById(R.id.totalfuelcosttextview);
+        avgMileage = (TextView) findViewById(R.id.averagegasmileagetextview);
+        enviroImpact = (TextView) findViewById(R.id.environmentalimpacttextview);
+
+
     }
+    //TODO reassign TextView objects with proper values for the selected report
+    // someTextView.setText(PR.get(attribute) + " units")
+
 }
